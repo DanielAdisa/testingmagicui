b104660ea150920631cf8761d8dc8d88a3f442f1
Your Name <you@example.com>
2024-11-04 02:17:45 +0100

    hkbn

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -8,16 +8,19 @@ import doom from "../assets/projects/doom.png"
 import oglav from "../assets/projects/oglav.png"
 import { PROJECTS } from "../constants";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const Gallery = () => {
   return (
-    <div className="mt-[30px] p-4">
+    <div className="mt-[30px] md:p-0 p-4  gap-4 ">
+        <h1 className=" text-2xl md:p-0 font-bold font-grey-bold mb-[25px]">Gallery</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {PROJECTS.map((item,index) => (
-            <div key={index}>
-                <Link href={item.link}>
-                <Image className="h-auto max-w-full rounded-lg" src={item.image}  alt=""></Image>
-                <h2>{item.title}</h2>
+            <div key={index} className="">
+                <Link href={item.link} className="">
+                    <h1 className=" hidden md:block text-center">{item.title}</h1>
+                    <Image className="h-auto max-w-full rounded-lg" src={item.image}  alt=""></Image>
+                    <Button className=" md:hidden w-full h-auto font-grey-med mt-2 p-4 ">{item.title}</Button>
                 </Link>
             </div>
         ))}
